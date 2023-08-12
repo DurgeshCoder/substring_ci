80abb489c5b3cb4cd293e8c21283994e5d12790a
durgesh tiwari <55891143+learncodewithdurgesh@users.noreply.github.com>
2023-08-12 11:12:12 +0530

    home page setup

--- app/Controllers/PageController.php
+++ app/Controllers/PageController.php
@@ -3,19 +3,45 @@
 namespace App\Controllers;
 
 
+use App\Models\Emp;
+
 class PageController extends BaseController
 {
 
 
-    public function index()
+    public function index(): string
     {
         return view("page_view");
     }
 
-    public function about()
+    public function about(): string
     {
-        return view("about_view");
+        $data = ["friends" => [
+
+            new Emp(12, "Gautam", "215235", "Lucknow"),
+            new Emp(13, "Ankit", "215235", "Lucknow"),
+            new Emp(14, "Uttam", "215235", "Lucknow"),
+            new Emp(15, "Durgesh", "215235", "Lucknow"),
+
+        ]];
+        return view("about_view", $data);
     }
 
+    public function services(): string
+    {
+        $data = ['pageTitle' => 'Services Title'];
+        return view('services_view', $data);
+    }
+
+
+    public function contact(): string
+    {
+        return view("contact_view");
+    }
+
+    public function testing(): string
+    {
+        return view("test");
+    }
 
 }
---
+++ app/Views/contact_view.php
@@ -0,0 +1,29 @@
+<?= $this->extend('default') ?>
+
+
+<!--pageTitle section start-->
+
+<?= $this->section('pageTitle') ?>
+Contact US | Substring Technologies
+<?= $this->endSection() ?>
+<!--pageTitle section end-->
+<!--start content section-->
+<?= $this->section('content') ?>
+
+
+<?= $this->include("page_section/hero_section"); ?>
+<?= $this->include("page_section/about_section"); ?>
+<?= $this->include("page_section/values_section"); ?>
+<?= $this->include("page_section/counts_section"); ?>
+<?= $this->include("page_section/features_section"); ?>
+
+
+
+
+
+
+
+<?= $this->endSection() ?>
+<!--end content section-->
+
+
