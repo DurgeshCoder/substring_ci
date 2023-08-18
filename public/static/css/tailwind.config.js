baa9855f2fa5a2c7ff4e6b36a9ecf5a6dee5cf4a
durgesh tiwari <55891143+LearnCodeWithDurgesh@users.noreply.github.com>
2023-08-18 12:17:59 +0530

    template change

--- public/static/css/tailwind.config.js
+++ public/static/css/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["../../../app/Views/**/*.{html,js,php}",], darkMode: 'false', theme: {
+    content: ["../../../app/Views/**/*.{html,js,php}",],
+    // darkMode:'false'
+    theme: {
         extend: {},
     }, plugins: [],
 }
