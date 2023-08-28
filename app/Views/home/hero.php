818802f4fc3fd9db52b6098fd2ef5a9f1d0738d6
durgesh tiwari <55891143+LearnCodeWithDurgesh@users.noreply.github.com>
2023-08-28 16:18:25 +0530

    hero section updated

--- app/Views/home/hero.php
+++ app/Views/home/hero.php
@@ -1,61 +1,62 @@
 <!-- banner started -->
 
 <div class="text-black  dark:text-white dark:bg-black bg-black  h-[644px]">
-<div class="relative">
-    <video autoplay loop muted class="absolute inset-0 w-full h-[644px] object-cover opacity-25 ">
-        <source src="static/img/banner_video.mp4" type="video/mp4">
-        Your browser does not support the video tag.
-    </video>
-  <div class="container mx-auto flex flex-col md:flex-row justify-center w-full items-center py-2  h-[644px] dark:bg-black">
-      <div class="flex text-center   flex-col  justify-center items-center">
-                  <h1 data-aos="fade-up"
-                      class="max-w-2xl mb-4 dark:text-white text-white text-4xl font-semibold tracking-tight leading-none md:text-3xl xl:text-6xl ">
-                      Where Code Meets Creativity: Empowering Your Digital Future.</h1>
-                  <p data-aos="fade-up" data-aos-delay="100"
-                           class="max-w-2xl mb-6 font-light text-white lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Your
-                              Vision, Our Code: Building Tomorrow's Solutions Today.</p>
-                  <div>
-                                      <a href="https://learncodewithdurgesh.com/" target="_blank"
+    <div class="relative">
+        <video autoplay loop muted class="absolute inset-0 w-full h-[644px] object-cover opacity-25  z-0 ">
+            <source src="static/img/banner_video.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+        <div class=" bg-[#rdba(0,0,0,0.9)] absolute z-10 mx-auto inset-0  flex flex-col md:flex-row justify-center w-full items-center py-2  h-[644px] ">
+            <div class="flex text-center   flex-col  justify-center items-center">
+                <h1 data-aos="fade-up"
+                    class="  max-w-2xl mb-4 dark:text-white text-white text-4xl font-semibold tracking-tight leading-none md:text-3xl xl:text-6xl ">
+                    Where Code Meets Creativity: Empowering Your Digital Future.</h1>
+                <p data-aos="fade-up" data-aos-delay="100"
+                   class="max-w-2xl mb-6 font-light text-white lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Your
+                    Vision, Our Code: Building Tomorrow's Solutions Today.</p>
+                <div>
+                    <a href="https://learncodewithdurgesh.com/" target="_blank"
 
-                                         data-aos="fade-up" data-aos-delay="200"
-                                         class="inline-flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded text-lg px-8 py-2 text-center xl:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                          Get Started
-                                          <i class="fa-solid fa-arrow-right fa-beat fa-sm mt-[15px] mx-2" style="color:white;"></i>
+                       data-aos="fade-up" data-aos-delay="200"
+                       class="inline-flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded text-lg px-8 py-2 text-center xl:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Get Started
+                        <i class="fa-solid fa-arrow-right fa-beat fa-sm mt-[15px] mx-2" style="color:white;"></i>
 
-                                      </a>
-                  </div>
-                    </div>
+                    </a>
                 </div>
+            </div>
+        </div>
 
 
-              </div>
+    </div>
 
 
-                  <!--    <div class=" container mx-auto flex  flex-col md:flex-row lg:gap-x-40 xl:gap-x-32 justify-center w-full align-center   py-2  md:py-16 px-8   ">-->
-<!--        <div class="flex  md:px-12 mt-5 text-center md:mt-5 md:text-start flex-col  justify-center">-->
-<!--            <h1 data-aos="fade-up"-->
-<!--                class="max-w-2xl mb-4 dark:text-blue-50 text-blue-900 text-4xl font-semibold tracking-tight leading-none md:text-3xl xl:text-6xl ">-->
-<!--                Where Code Meets Creativity: Empowering Your Digital Future.</h1>-->
-<!--            <p data-aos="fade-up" data-aos-delay="100"-->
-<!--               class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Your-->
-<!--                Vision, Our Code: Building Tomorrow's Solutions Today.</p>-->
-<!--            <div>-->
-<!--                <a href="https://learncodewithdurgesh.com/" target="_blank"-->
-<!---->
-<!--                   data-aos="fade-up" data-aos-delay="200"-->
-<!--                   class="inline-flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded text-lg px-8 py-2 text-center xl:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">-->
-<!--                    Get Started-->
-<!--                    <i class="fa-solid fa-arrow-right fa-beat fa-sm mt-[15px] mx-2" style="color:white"></i>-->
-<!---->
-<!--                </a>-->
-<!--            </div>-->
-<!--        </div>-->
-<!---->
-<!--        <div class="xl:mt-2  justify-center order-first md:order-last  flex aos-init "-->
-<!--             data-aos="zoom-in">-->
-<!--            <img data-aos="fade-up" class="w-2/3" src=" --><?php //= base_url('static/img/banner.gif') ?><!-- "-->
-<!--                 alt="substring technologies">-->
-<!--        </div>-->
-<!--    </div>-->
+    <!--    <div class=" container mx-auto flex  flex-col md:flex-row lg:gap-x-40 xl:gap-x-32 justify-center w-full align-center   py-2  md:py-16 px-8   ">-->
+    <!--        <div class="flex  md:px-12 mt-5 text-center md:mt-5 md:text-start flex-col  justify-center">-->
+    <!--            <h1 data-aos="fade-up"-->
+    <!--                class="max-w-2xl mb-4 dark:text-blue-50 text-blue-900 text-4xl font-semibold tracking-tight leading-none md:text-3xl xl:text-6xl ">-->
+    <!--                Where Code Meets Creativity: Empowering Your Digital Future.</h1>-->
+    <!--            <p data-aos="fade-up" data-aos-delay="100"-->
+    <!--               class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Your-->
+    <!--                Vision, Our Code: Building Tomorrow's Solutions Today.</p>-->
+    <!--            <div>-->
+    <!--                <a href="https://learncodewithdurgesh.com/" target="_blank"-->
+    <!---->
+    <!--                   data-aos="fade-up" data-aos-delay="200"-->
+    <!--                   class="inline-flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded text-lg px-8 py-2 text-center xl:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">-->
+    <!--                    Get Started-->
+    <!--                    <i class="fa-solid fa-arrow-right fa-beat fa-sm mt-[15px] mx-2" style="color:white"></i>-->
+    <!---->
+    <!--                </a>-->
+    <!--            </div>-->
+    <!--        </div>-->
+    <!---->
+    <!--        <div class="xl:mt-2  justify-center order-first md:order-last  flex aos-init "-->
+    <!--             data-aos="zoom-in">-->
+    <!--            <img data-aos="fade-up" class="w-2/3" src=" -->
+    <?php //= base_url('static/img/banner.gif') ?><!-- "-->
+    <!--                 alt="substring technologies">-->
+    <!--        </div>-->
+    <!--    </div>-->
 </div>
 <!-- banner ended -->
