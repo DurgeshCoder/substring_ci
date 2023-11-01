69da9e1f95ac83ca3918193ebe581b53b7f80c68
Ashutosh <ashutosh010sharma@gmail.com>
2023-11-01 14:12:53 +0530

    worked on contect section

--- app/Views/training_view.php
+++ app/Views/training_view.php
@@ -19,43 +19,43 @@ Courses offered by Substring Technologies
 
 <!-- banner section starts -->
 <div style="--swiper-navigation-color: #fff; --swiper-pagination-color: #fff" class="swiper mySwiper1 h-[50vh]">
-<div class="parallax-bg" style="background-image: url('<?= base_url('images/course_bg.jpg') ?>'); "  data-swiper-parallax="-23%"></div>
+<div class="parallax-bg" style="background-image: url('<?= base_url('images/course_bg .jpg') ?>');" data-swiper-parallax="-23%"></div>
 
         
-    <div class="swiper-wrapper text-center">
-      <div class="swiper-slide grid grid-cols-1 md:grid-cols-2 -gap-4 lg:px-44 bg-black bg-opacity-20 transform transition duration-1000 hover:bg-opacity-40">
+    <div class="swiper-wrapper text-center bg-black bg-opacity-20 transform transition duration-700 hover:bg-opacity-40">
+      <div class="swiper-slide grid grid-cols-1 md:grid-cols-2 gap-4">
         <div class="grid place-items-center lg:-mt-32" data-swiper-parallax="-300">
-            <div class="text-center px-8 md:text-left md:px-10 lg:mt-20 ">
-              <h1 class="text-2xl md:text-4xl font-bold my-0 text-center animation" data-aos="fade-right" data-aos-duration="1000">Where Algorithms Meet Aspiration</h1>
-              <p class="text-sm md:text-lg my-0 font-semibold mt-4 text-center animation" data-aos="fade-left" data-aos-duration="1000">Emphasizes the role of algorithms and problem-solving skills in fulfilling one's aspirations and goals.</p>
+            <div class="text-center px-8 md:text-left md:px-10 lg:px-20 lg:mt-20">
+              <h1 class="text-2xl md:text-4xl font-bold my-0">Centered Heading</h1>
+              <p class="text-sm md:text-lg my-0 font-semibold mt-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi, laborum nisi? Neque fuga delectus possimus voluptas praesentium ab tenetur deleniti. Dolor fugit et dolorum dolore, doloremque quasi nihil aliquid a ad laudantium optio illo autem voluptatem quidem. Cupiditate, illum corrupti.Centered paragraph text.</p>
             </div>
           </div>
         <div class=" grid place-items-center">
-            <img src="images/undraw_2.svg" class="h-[150px] md:h-[250px] xl:h-[400px] animatiom" data-aos="flip-right" alt="">
+            <img src="images/undraw_2.svg" class="h-[150px] md:h-[250px] xl:h-[400px]" alt="">
         </div>
        
       </div>
-      <div class="swiper-slide grid grid-cols-1 md:grid-cols-2 -gap-4 lg:px-44 bg-black bg-opacity-20  transform transition duration-1000 hover:bg-opacity-40">
+      <div class="swiper-slide grid grid-cols-1 md:grid-cols-2 gap-4 bg-black bg-opacity-20 transform transition duration-700 hover:bg-opacity-40">
         <div class="grid place-items-center lg:-mt-32" data-swiper-parallax="-300">
-            <div class="text-center px-8 md:text-left md:px-10 lg:mt-20">
-              <h1 class="text-2xl md:text-4xl font-bold my-0 text-center animation" data-aos="fade-right" data-aos-duration="1000">Data-Driven Dreams, Code-Powered Reality</h1>
-              <p class="text-sm md:text-lg my-0 font-semibold mt-4 text-center animation" data-aos="fade-left" data-aos-duration="1000">Conveys the idea that the world of data and code can transform dreams into a tangible and achievable reality.</p>
+            <div class="text-center px-8 md:text-left md:px-10 lg:px-20 lg:mt-20">
+              <h1 class="text-2xl md:text-4xl font-bold my-0">Centered Heading</h1>
+              <p class="text-sm md:text-lg my-0 font-semibold mt-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi, laborum nisi? Neque fuga delectus possimus voluptas praesentium ab tenetur deleniti. Dolor fugit et dolorum dolore, doloremque quasi nihil aliquid a ad laudantium optio illo autem voluptatem quidem. Cupiditate, illum corrupti.Centered paragraph text.</p>
             </div>
           </div>
         <div class=" grid place-items-center">
-            <img src="images/undraw_3.svg" class="h-[150px] md:h-[250px] xl:h-[400px] animation" data-aos="flip-left" alt="">
+            <img src="images/undraw_3.svg" class="h-[150px] md:h-[250px] xl:h-[400px]" alt="">
         </div>
        
       </div>
-      <div class="swiper-slide grid grid-cols-1 md:grid-cols-2 -gap-4 lg:px-44 bg-black bg-opacity-20  transform transition duration-1000 hover:bg-opacity-40">
+      <div class="swiper-slide grid grid-cols-1 md:grid-cols-2 gap-4 bg-black bg-opacity-20 transform transition duration-700 hover:bg-opacity-40">
         <div class="grid place-items-center lg:-mt-32" data-swiper-parallax="-300">
-            <div class="text-center px-8 md:text-left md:px-10  lg:mt-20">
-              <h1 class="text-2xl md:text-4xl font-bold my-0 text-center animation" data-aos="fade-right" data-aos-duration="1000">Smart Learning for Smart Minds.</h1>
-              <p class="text-sm md:text-lg my-0 font-semibold mt-4 text-center animation" data-aos="fade-left" data-aos-duration="1000">Tailored education for fast learners, harnessing innate intelligence for advanced, stimulating, and innovative learning.</p>
+            <div class="text-center px-8 md:text-left md:px-10 lg:px-20 lg:mt-20">
+              <h1 class="text-2xl md:text-4xl font-bold my-0">Centered Heading</h1>
+              <p class="text-sm md:text-lg my-0 font-semibold mt-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi, laborum nisi? Neque fuga delectus possimus voluptas praesentium ab tenetur deleniti. Dolor fugit et dolorum dolore, doloremque quasi nihil aliquid a ad laudantium optio illo autem voluptatem quidem. Cupiditate, illum corrupti.Centered paragraph text.</p>
             </div>
           </div>
         <div class=" grid place-items-center">
-            <img src="images/undraw_1.svg" class="h-[150px] md:h-[250px] xl:h-[400px] animation" data-aos="flip-right" alt="">
+            <img src="images/undraw_1.svg" class="h-[150px] md:h-[250px] xl:h-[400px]" alt="">
         </div>
        
       </div>
@@ -79,13 +79,13 @@ Courses offered by Substring Technologies
     <div class=" px-4 xl:px-32 mx-auto"> 
         <form>   
             <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-            <div class="relative max-w-4xl mx-auto">
+            <div class="relative">
                 <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                     <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                     </svg>
                 </div>
-                <input type="search" id="default-search" class="block shadow-lg w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-2xl bg-gray-50 focus:ring-blue-900  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-700 dark:focus:border-blue-500" placeholder="Search Courses . . ." required>
+                <input type="search" id="default-search" class="block shadow-lg w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-2xl bg-gray-50 focus:ring-blue-900 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-700 dark:focus:border-blue-500" placeholder="Search Courses . . ." required>
                 <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded text-sm px-8 py-2  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
             </div>
         </form>
@@ -105,10 +105,10 @@ Courses offered by Substring Technologies
         <?php foreach($courses as $course): ?>
             <div
               class="mx-3 mt-6 flex flex-col rounded-xl bg-black bg-opacity-10 hover:bg-opacity-60 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 sm:shrink-0 sm:grow sm:basis-0 transform duration-500 hover:-translate-y-2 cursor-pointer border-b-[7px] border-blue-700 group">
-              <div class="overflow-hidden bg-cover bg-no-repeat">
-              <img class="rounded-t-lg hover:scale-[1.08] transition duration-700 ease-in-out h-64 w-full " src="<?= file_exists(FCPATH . "cover_images/" . $course['cover_image']) ? base_url("cover_images/" . $course['cover_image']) : base_url("cover_images/church.jpg") ?>" alt="Course Image" />
+              <a href="#!">
+              <img class="rounded-t-lg" src="<?= file_exists(FCPATH . "cover_images/" . $course['cover_image']) ? base_url("cover_images/" . $course['cover_image']) : base_url("cover_images/church.jpg") ?>" alt="Course Image" />
 
-           </div>
+              </a>
               <div class="p-6 transform duration-500 hover:-translate-y-2 cursor-pointer">
                 <h5
                   class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50 group-hover:text-white">
@@ -123,7 +123,7 @@ Courses offered by Substring Technologies
                   <a href="<?php echo base_url('course_view/'.$course['slug']) ?>">
                   <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 ">
                         <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group- hover:bg-opacity-0">
-                            Join Training
+                            Start Learning
                         </span>
                     </button></a>
                     
